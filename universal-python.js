fc6bb84510d8bba693c1fcf8c0c8bcdd8a0bdc0d
Muffin <muffin@mailbox.org>
2024-01-03 22:54:38 -0600

    Make it work on windows

--- universal-python.js
+++ universal-python.js
@@ -14,15 +14,26 @@ function which(command) {
 }
 
 function run(command, args) {
-  console.log('universal-python: running ' + command + ' with arguments ' + args.join(', '));
+  console.log('universal-python: Running ' + command + ' with arguments ' + args.join(', '));
   var subprocess = child_process.spawn(command, args, {
     windowsHide: true,
     shell: false,
     stdio: 'inherit'
   });
   subprocess.on('exit', function(code) {
-    console.log('universal-python: exited with code ' + code);
+    // Microsoft Store install shim exits with code 9009
+    if (process.platform === 'win32' && code === 9009) {
+      console.log('universal-python: Attempted to start python, but got the Microsoft Store installation shim. Install python from the Microsoft Store or python.org. (code 9009)')
+    } else if (code === 0) {
+      console.log('universal-python: Success');
+    } else {
+      console.log('universal-python: Failed with code ' + code);
+    }
     process.exit(code);
+  });
+  subprocess.on('error', function(error) {
+    console.log('universal-python: Error starting python; please ensure you have python installed: ' + error);
+    process.exit(1);
   });
   process.on('SIGINT', function() {
     subprocess.kill('SIGINT');
@@ -31,29 +42,38 @@ function run(command, args) {
 
 var argv = process.argv.slice(2);
 
-// Linux distributions, macOS, and Microsoft Store install python 3 as python3
-// This is the happy path
-var python3 = which('python3');
-if (python3) {
-  run(python3, argv);
-} else {
-  // Windows installers from python.org install the py launcher
-  var py = which('py');
+if (process.platform === 'win32') {
+  // Python in Windows is weird. Most commonly it will be installed using the python.org installers or
+  // from the Microsoft Store. Interestingly the Microsoft Store shims always exist on PATH and are
+  // also weird fake files so Node.js' fs does not work very well with them.
+  // Python.org installers will usually install the py launcher, so we'll try that first so that we
+  // don't open up the Microsoft Store for people who installed Python from there.
+  var py = which('py.exe');
   if (py) {
     run(py, ['-3'].concat(argv));
   } else {
-    // Sometimes it only gets installed as "python"
-    // Don't know what version it will be, but 2 and 3 both work so it doesn't matter
+    // This will either run the python installation from PATH or the Microsoft Store install shim.
+    // Don't know what version we'll get, but 2 and 3 both work so it doesn't matter.
+    run('python.exe', argv);
+  }
+} else {
+  // Python 3 is usually installed as python3
+  var python3 = which('python3');
+  if (python3) {
+    run(python3, argv);
+  } else {
+    // Sometimes it might only be installed as python
+    // Don't know what version we'll get, but 2 and 3 both work so it doesn't matter
     var python = which('python');
     if (python) {
       run(python, argv);
     } else {
-      // Some weird systems might only have python 2 installed as python2
+      // Some old systems might only have python 2
       var python2 = which('python2');
       if (python2) {
         run(python2, argv);
       } else {
-        console.log("Could not find any python executables on your PATH environment variable");
+        console.log("universal-python: Could not find python on your PATH; please ensure you have python installed.");
         process.exit(1);
       }
     }
